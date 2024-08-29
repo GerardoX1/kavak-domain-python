2ad1e14ff5187661a0c14944ffe27c62c8e9d77f
Luis <gerardo.fosado@cargamos.com>
2024-08-29 11:03:18 -0600

    updater model

--- kavak/services/v1/vehicles.py
+++ kavak/services/v1/vehicles.py
@@ -1,10 +1,10 @@
-from typing import List, Optional, Tuple, Union
+from typing import Optional
 
 from kavak.models.v1.vehicles import VehiclesModel
 from kavak.services.base_services.base_service import BaseService
 
 
-class AppVersionService(BaseService[VehiclesModel]):
+class VehiclesService(BaseService[VehiclesModel]):
     __entity_model__ = VehiclesModel
 
     def __init__(self, repository: object, verbose: bool = False, *args, **kwargs):
@@ -17,18 +17,3 @@ class AppVersionService(BaseService[VehiclesModel]):
         if not query_result:
             return None
         return self.__entity_model__(**query_result)
-
-    def paginated_query(
-        self,
-        conditions: List[tuple] = None,
-        page: int = 1,
-        limit: int = 50,
-        sort: Optional[List[Tuple[str, int]]] = None,
-    ) -> Union[Tuple[int, List[dict]], None]:
-        count, query_result = self._query_paginated(
-            page=page, limit=limit, and_conditions=conditions, sort=sort
-        )
-        return count, [
-            self.__entity_model__(**vehicle).model_dump(by_alias=True)
-            for vehicle in query_result
-        ]
